482a90eda9354d603c8e15f277ba37974db051a1
a NonLethal Jerk <anonlethaljerk@gmail.com>
2019-05-21 13:54:22 -0400

    combining my memories into a pilot

---
+++ clamshell_box_template.jsx
@@ -0,0 +1,316 @@
+//
+// created by JK Keller, dilettante coder
+// http://jk-keller.com
+//
+
+#target illustrator
+
+var docRef = app.activeDocument;
+var newLayerRef = docRef.layers.add();
+
+// MILLIMETERS TO POINTS CONSTANT
+var _TO_POINTS = 2.834645669;
+
+// PROMPTS FOR BOX VALUES
+var pBoxThickness = prompt("Cardboard Thickness (mm)", "2")*_TO_POINTS;
+var pBoxLength = prompt("Interior Length (mm)", "191")*_TO_POINTS;
+var pBoxDepth = prompt("Interior Depth (mm)",  "62")*_TO_POINTS;
+var pBoxHeight = prompt("Interior Height (mm)", "62")*_TO_POINTS;
+
+// INITIALIZE STROKE PROPERTIES
+var strokeW = 1;
+var foldColor = new RGBColor();
+foldColor.red = 255;
+foldColor.green = 23;
+foldColor.blue = 24;
+var cutColor = new RGBColor();
+cutColor.red = 24;
+cutColor.green = 255;
+cutColor.blue = 99;
+
+// SETUP VALUES FOR BOX SIDES
+var x_start = 10 + pBoxThickness + pBoxDepth + pBoxLength/2;
+/* var y_start = -1 * (pBoxHeight*3 + pBoxDepth*2 + pBoxThickness*10); */
+var y_start = -300 * _TO_POINTS;
+
+// ILLUSTRATOR UNITS START AT TOP LEFT (SO - IS ADDITIVE)
+var x_front_frontflap = pBoxLength/2 + pBoxThickness/2 + pBoxThickness;
+var x_frontflap_edge  = x_front_frontflap + pBoxDepth; // maybe minus some
+
+var x_bottom_top_edge = x_front_frontflap + pBoxThickness + pBoxThickness/2;
+
+var x_back_side       = x_front_frontflap + pBoxThickness;
+var x_side_edge       = x_back_side + pBoxDepth + pBoxThickness/2;
+var x_sidenotch_1     = x_bottom_top_edge + pBoxDepth/3;
+var x_sidenotch_2     = x_sidenotch_1 + pBoxDepth/3;
+
+var x_frontcover_frontcoverflap  = x_front_frontflap + pBoxThickness;
+var x_frontcoverflap_edge        = x_frontcover_frontcoverflap + pBoxDepth/2;
+
+
+var y_front_edge      = y_start;
+var y_front_bottom    = y_front_edge     + pBoxHeight + pBoxThickness/2;
+var y_bottom_back     = y_front_bottom   + pBoxDepth  + pBoxThickness/2;
+var y_back_top        = y_bottom_back    + pBoxHeight + pBoxThickness;
+var y_top_frontcover  = y_back_top       + pBoxDepth  + pBoxThickness/2;
+var y_frontcover_edge = y_top_frontcover + pBoxHeight + pBoxThickness;
+
+var y_side_sidenotchflap         = y_bottom_back + pBoxThickness/2;
+var y_side_sidefoldover          = y_back_top    - pBoxThickness;
+var y_sidefoldover_sidetoothflap = y_back_top    + pBoxThickness;
+
+var y_frontflap_bottomedge = y_front_bottom         - pBoxThickness;
+var y_frontflap_clearance  = y_frontflap_bottomedge - pBoxThickness;
+
+var y_sidenotchflap_topedge    = Math.max( y_side_sidenotchflap - pBoxLength/2 - pBoxThickness/2 - pBoxThickness*2 , y_frontflap_bottomedge);
+var y_sidenotch_topedge        = y_side_sidenotchflap - pBoxThickness*2;
+var y_sidetoothflap_bottomedge = y_sidefoldover_sidetoothflap + pBoxHeight + pBoxThickness/2;
+var y_sidetooth_bottomedge     = y_sidetoothflap_bottomedge + pBoxThickness + pBoxThickness/2;
+
+var y_frontcoverflap_topedge         = y_top_frontcover + pBoxThickness;
+var y_frontcoverflap_topclearnace    = y_top_frontcover + pBoxThickness*3;
+var y_frontcoverflap_bottomclearance = y_frontcover_edge - pBoxThickness*2;
+
+
+// MAKE THE PATHS
+var front_bottom_fold = newLayerRef.pathItems.add();
+front_bottom_fold.filled = false;
+front_bottom_fold.stroked = true;
+front_bottom_fold.strokeWidth = strokeW;
+front_bottom_fold.strokeColor = foldColor;
+front_bottom_fold.closed = false;
+front_bottom_fold.setEntirePath(new Array(
+	[x_start+x_bottom_top_edge, y_front_bottom],
+	[x_start-x_bottom_top_edge, y_front_bottom]
+));
+var bottom_back_fold = newLayerRef.pathItems.add();
+bottom_back_fold.filled = false;
+bottom_back_fold.stroked = true;
+bottom_back_fold.strokeWidth = strokeW;
+bottom_back_fold.strokeColor = foldColor;
+bottom_back_fold.closed = false;
+bottom_back_fold.setEntirePath(new Array(
+	[x_start+x_bottom_top_edge,  y_bottom_back],
+	[x_start-x_bottom_top_edge, y_bottom_back]
+));
+var back_top_fold = newLayerRef.pathItems.add();
+back_top_fold.filled = false;
+back_top_fold.stroked = true;
+back_top_fold.strokeWidth = strokeW;
+back_top_fold.strokeColor = foldColor;
+back_top_fold.closed = false;
+back_top_fold.setEntirePath(new Array(
+	[x_start+x_bottom_top_edge,  y_back_top],
+	[x_start-x_bottom_top_edge, y_back_top]
+));
+var top_frontcover_fold = newLayerRef.pathItems.add();
+top_frontcover_fold.filled = false;
+top_frontcover_fold.stroked = true;
+top_frontcover_fold.strokeWidth = strokeW;
+top_frontcover_fold.strokeColor = foldColor;
+top_frontcover_fold.closed = false;
+top_frontcover_fold.setEntirePath(new Array(
+	[x_start+x_bottom_top_edge, y_top_frontcover],
+	[x_start-x_bottom_top_edge, y_top_frontcover]
+));
+var sidenotchflap_side_fold = newLayerRef.pathItems.add();
+sidenotchflap_side_fold.filled = false;
+sidenotchflap_side_fold.stroked = true;
+sidenotchflap_side_fold.strokeWidth = strokeW;
+sidenotchflap_side_fold.strokeColor = foldColor;
+sidenotchflap_side_fold.closed = false;
+sidenotchflap_side_fold.setEntirePath(new Array(
+	[x_start+x_back_side,  y_side_sidenotchflap],
+	[x_start+x_side_edge,  y_side_sidenotchflap]
+));
+var side_sidefoldover_fold = newLayerRef.pathItems.add();
+side_sidefoldover_fold.filled = false;
+side_sidefoldover_fold.stroked = true;
+side_sidefoldover_fold.strokeWidth = strokeW;
+side_sidefoldover_fold.strokeColor = foldColor;
+side_sidefoldover_fold.closed = false;
+side_sidefoldover_fold.setEntirePath(new Array(
+	[x_start+x_back_side,  y_side_sidefoldover],
+	[x_start+x_side_edge,  y_side_sidefoldover]
+));
+var sidefoldover_sidetoothflap_fold = newLayerRef.pathItems.add();
+sidefoldover_sidetoothflap_fold.filled = false;
+sidefoldover_sidetoothflap_fold.stroked = true;
+sidefoldover_sidetoothflap_fold.strokeWidth = strokeW;
+sidefoldover_sidetoothflap_fold.strokeColor = foldColor;
+sidefoldover_sidetoothflap_fold.closed = false;
+sidefoldover_sidetoothflap_fold.setEntirePath(new Array(
+	[x_start+x_bottom_top_edge,  y_sidefoldover_sidetoothflap],
+	[x_start+x_side_edge,  y_sidefoldover_sidetoothflap]
+));
+var neg_sidenotchflap_side_fold = newLayerRef.pathItems.add();
+neg_sidenotchflap_side_fold.filled = false;
+neg_sidenotchflap_side_fold.stroked = true;
+neg_sidenotchflap_side_fold.strokeWidth = strokeW;
+neg_sidenotchflap_side_fold.strokeColor = foldColor;
+neg_sidenotchflap_side_fold.closed = false;
+neg_sidenotchflap_side_fold.setEntirePath(new Array(
+	[x_start-x_back_side,  y_side_sidenotchflap],
+	[x_start-x_side_edge,  y_side_sidenotchflap]
+));
+var neg_side_sidefoldover_fold = newLayerRef.pathItems.add();
+neg_side_sidefoldover_fold.filled = false;
+neg_side_sidefoldover_fold.stroked = true;
+neg_side_sidefoldover_fold.strokeWidth = strokeW;
+neg_side_sidefoldover_fold.strokeColor = foldColor;
+neg_side_sidefoldover_fold.closed = false;
+neg_side_sidefoldover_fold.setEntirePath(new Array(
+	[x_start-x_back_side,  y_side_sidefoldover],
+	[x_start-x_side_edge,  y_side_sidefoldover]
+));
+var neg_sidefoldover_sidetoothflap_fold = newLayerRef.pathItems.add();
+neg_sidefoldover_sidetoothflap_fold.filled = false;
+neg_sidefoldover_sidetoothflap_fold.stroked = true;
+neg_sidefoldover_sidetoothflap_fold.strokeWidth = strokeW;
+neg_sidefoldover_sidetoothflap_fold.strokeColor = foldColor;
+neg_sidefoldover_sidetoothflap_fold.closed = false;
+neg_sidefoldover_sidetoothflap_fold.setEntirePath(new Array(
+	[x_start-x_bottom_top_edge,  y_sidefoldover_sidetoothflap],
+	[x_start-x_side_edge,  y_sidefoldover_sidetoothflap]
+));
+var back_side_fold = newLayerRef.pathItems.add();
+back_side_fold.filled = false;
+back_side_fold.stroked = true;
+back_side_fold.strokeWidth = strokeW;
+back_side_fold.strokeColor = foldColor;
+back_side_fold.closed = false;
+back_side_fold.setEntirePath(new Array(
+	[x_start+x_back_side,  y_side_sidenotchflap],
+	[x_start+x_back_side,  y_side_sidefoldover]
+));
+var neg_back_side_fold = newLayerRef.pathItems.add();
+neg_back_side_fold.filled = false;
+neg_back_side_fold.stroked = true;
+neg_back_side_fold.strokeWidth = strokeW;
+neg_back_side_fold.strokeColor = foldColor;
+neg_back_side_fold.closed = false;
+neg_back_side_fold.setEntirePath(new Array(
+	[x_start-x_back_side,  y_side_sidenotchflap],
+	[x_start-x_back_side,  y_side_sidefoldover]
+));
+var front_frontflap_fold = newLayerRef.pathItems.add();
+front_frontflap_fold.filled = false;
+front_frontflap_fold.stroked = true;
+front_frontflap_fold.strokeWidth = strokeW;
+front_frontflap_fold.strokeColor = foldColor;
+front_frontflap_fold.closed = false;
+front_frontflap_fold.setEntirePath(new Array(
+	[x_start+x_front_frontflap,  y_front_edge],
+	[x_start+x_front_frontflap,  y_frontflap_bottomedge]
+));
+var neg_front_frontflap_fold = newLayerRef.pathItems.add();
+neg_front_frontflap_fold.filled = false;
+neg_front_frontflap_fold.stroked = true;
+neg_front_frontflap_fold.strokeWidth = strokeW;
+neg_front_frontflap_fold.strokeColor = foldColor;
+neg_front_frontflap_fold.closed = false;
+neg_front_frontflap_fold.setEntirePath(new Array(
+	[x_start-x_front_frontflap,  y_front_edge],
+	[x_start-x_front_frontflap,  y_frontflap_bottomedge]
+));
+var frontcover_frontcoverflap_fold = newLayerRef.pathItems.add();
+frontcover_frontcoverflap_fold.filled = false;
+frontcover_frontcoverflap_fold.stroked = true;
+frontcover_frontcoverflap_fold.strokeWidth = strokeW;
+frontcover_frontcoverflap_fold.strokeColor = foldColor;
+frontcover_frontcoverflap_fold.closed = false;
+frontcover_frontcoverflap_fold.setEntirePath(new Array(
+	[x_start+x_frontcover_frontcoverflap,  y_frontcoverflap_topedge],
+	[x_start+x_frontcover_frontcoverflap,  y_frontcover_edge]
+));
+var neg_frontcover_frontcoverflap_fold = newLayerRef.pathItems.add();
+neg_frontcover_frontcoverflap_fold.filled = false;
+neg_frontcover_frontcoverflap_fold.stroked = true;
+neg_frontcover_frontcoverflap_fold.strokeWidth = strokeW;
+neg_frontcover_frontcoverflap_fold.strokeColor = foldColor;
+neg_frontcover_frontcoverflap_fold.closed = false;
+neg_frontcover_frontcoverflap_fold.setEntirePath(new Array(
+	[x_start-x_frontcover_frontcoverflap,  y_frontcoverflap_topedge],
+	[x_start-x_frontcover_frontcoverflap,  y_frontcover_edge]
+));
+
+var box_outline_cut = newLayerRef.pathItems.add();
+box_outline_cut.filled = false;
+box_outline_cut.stroked = true;
+box_outline_cut.strokeWidth = strokeW;
+box_outline_cut.strokeColor = cutColor;
+box_outline_cut.closed = true;
+box_outline_cut.setEntirePath(new Array(
+	[x_start+x_frontflap_edge, y_front_edge],
+	[x_start+x_frontflap_edge, y_frontflap_clearance],
+	[x_start+x_front_frontflap, y_frontflap_bottomedge],
+	[x_start+x_bottom_top_edge, y_front_bottom],
+	[x_start+x_bottom_top_edge, y_bottom_back],
+	[x_start+x_back_side, y_side_sidenotchflap],
+	[x_start+x_bottom_top_edge, y_bottom_back],
+	[x_start+x_bottom_top_edge, y_sidenotchflap_topedge],
+	[x_start+x_side_edge, y_sidenotchflap_topedge],
+	[x_start+x_side_edge, y_sidetoothflap_bottomedge],
+	[x_start+x_sidenotch_2, y_sidetoothflap_bottomedge],
+	[x_start+x_sidenotch_2, y_sidetooth_bottomedge],
+	[x_start+x_sidenotch_1, y_sidetooth_bottomedge],
+	[x_start+x_sidenotch_1, y_sidetoothflap_bottomedge],
+	[x_start+x_bottom_top_edge, y_sidetoothflap_bottomedge],
+	[x_start+x_bottom_top_edge, y_back_top],
+	[x_start+x_back_side, y_side_sidefoldover],
+	[x_start+x_bottom_top_edge, y_back_top],
+	[x_start+x_bottom_top_edge, y_top_frontcover],
+	[x_start+x_frontcover_frontcoverflap, y_frontcoverflap_topedge],
+	[x_start+x_frontcoverflap_edge, y_frontcoverflap_topclearnace],
+	[x_start+x_frontcoverflap_edge, y_frontcoverflap_bottomclearance],
+	[x_start+x_frontcover_frontcoverflap, y_frontcover_edge],
+
+	[x_start-x_frontcover_frontcoverflap, y_frontcover_edge],
+	[x_start-x_frontcoverflap_edge, y_frontcoverflap_bottomclearance],
+	[x_start-x_frontcoverflap_edge, y_frontcoverflap_topclearnace],
+	[x_start-x_frontcover_frontcoverflap, y_frontcoverflap_topedge],
+	[x_start-x_bottom_top_edge, y_top_frontcover],
+	[x_start-x_bottom_top_edge, y_back_top],
+	[x_start-x_back_side, y_side_sidefoldover],
+	[x_start-x_bottom_top_edge, y_back_top],
+	[x_start-x_bottom_top_edge, y_sidetoothflap_bottomedge],
+	[x_start-x_sidenotch_1, y_sidetoothflap_bottomedge],
+	[x_start-x_sidenotch_1, y_sidetooth_bottomedge],
+	[x_start-x_sidenotch_2, y_sidetooth_bottomedge],
+	[x_start-x_sidenotch_2, y_sidetoothflap_bottomedge],
+	[x_start-x_side_edge, y_sidetoothflap_bottomedge],
+	[x_start-x_side_edge, y_sidenotchflap_topedge],
+	[x_start-x_bottom_top_edge, y_sidenotchflap_topedge],
+	[x_start-x_bottom_top_edge, y_bottom_back],
+	[x_start-x_back_side, y_side_sidenotchflap],
+	[x_start-x_bottom_top_edge, y_bottom_back],
+	[x_start-x_bottom_top_edge, y_front_bottom],
+	[x_start-x_front_frontflap, y_frontflap_bottomedge],
+	[x_start-x_frontflap_edge, y_frontflap_clearance],
+	[x_start-x_frontflap_edge, y_front_edge]
+));
+
+var sidenotch_cut = newLayerRef.pathItems.add();
+sidenotch_cut.filled = false;
+sidenotch_cut.stroked = true;
+sidenotch_cut.strokeWidth = strokeW;
+sidenotch_cut.strokeColor = cutColor;
+sidenotch_cut.closed = true;
+sidenotch_cut.setEntirePath(new Array(
+	[x_start+x_sidenotch_1,  y_side_sidenotchflap],
+	[x_start+x_sidenotch_2,  y_side_sidenotchflap],
+	[x_start+x_sidenotch_2,  y_sidenotch_topedge],
+	[x_start+x_sidenotch_1,  y_sidenotch_topedge]
+));
+var neg_sidenotch_cut = newLayerRef.pathItems.add();
+neg_sidenotch_cut.filled = false;
+neg_sidenotch_cut.stroked = true;
+neg_sidenotch_cut.strokeWidth = strokeW;
+neg_sidenotch_cut.strokeColor = cutColor;
+neg_sidenotch_cut.closed = true;
+neg_sidenotch_cut.setEntirePath(new Array(
+	[x_start-x_sidenotch_1,  y_side_sidenotchflap],
+	[x_start-x_sidenotch_2,  y_side_sidenotchflap],
+	[x_start-x_sidenotch_2,  y_sidenotch_topedge],
+	[x_start-x_sidenotch_1,  y_sidenotch_topedge]
+));
---
+++ corners.jsx
@@ -0,0 +1,153 @@
+//
+// created by A Nonlethal Jerk, dilettante coder
+//
+
+//
+// TO DO:
+//
+
+var colCyan = new CMYKColor;
+    colCyan.cyan = 100;
+    colCyan.magenta = 0;
+    colCyan.yellow = 0;
+    colCyan.black = 0;
+var colMagenta = new CMYKColor;
+    colMagenta.cyan = 0;
+    colMagenta.magenta = 100;
+    colMagenta.yellow = 0;
+    colMagenta.black = 0;
+var colyellow = new CMYKColor;
+    colyellow.cyan = 0;
+    colyellow.magenta = 0;
+    colyellow.yellow = 100;
+    colyellow.black = 0;
+var colBlack = new CMYKColor;
+    colBlack.cyan = 0;
+    colBlack.magenta = 0;
+    colBlack.yellow = 0;
+    colBlack.black = 100;
+var colGreen = new CMYKColor;
+    colGreen.cyan = 100;
+    colGreen.magenta = 0;
+    colGreen.yellow = 100;
+    colGreen.black = 0;
+var colPurple = new CMYKColor;
+    colPurple.cyan = 100;
+    colPurple.magenta = 100;
+    colPurple.yellow = 0;
+    colPurple.black = 0;
+var colOrange = new CMYKColor;
+    colOrange.cyan = 0;
+    colOrange.magenta = 100;
+    colOrange.yellow = 100;
+    colOrange.black = 0;
+
+var selRef = activeDocument.selection;
+
+var doc = app.documents[0];
+doc.defaultStrokeWidth = .25;
+doc.defaultStrokeCap = StrokeCap.ROUNDENDCAP;
+var layr = doc.layers.add();
+
+var grid = [];
+var gridXs = 30;
+var gridYs = 40;
+var gridWH = 20;
+
+for (i=0; i<gridYs; i++) {
+    var tempXs = [ 1 ];
+    for (j=0; j<gridXs-2; j++) {
+        if (i === 0 || i === gridYs-1) {
+            tempXs.push(1);
+        } else {
+            tempXs.push(0);
+        }
+    }
+    tempXs.push(1);
+    grid.push(tempXs)
+}
+
+for (i=0; i<20; i++) {
+    var signX = 1;
+    var start_x = Math.floor(Math.random()*(gridXs-4))+2;
+    if (Math.random() < .5) { signX = -1; };
+    var end_x   = start_x + signX;
+    var signY = 1;
+    var start_y = Math.floor(Math.random()*(gridYs-4))+2;
+    if (Math.random() < .5) { signY = -1; };
+    var end_y   = start_y + signY;
+
+
+    if (grid[start_y][start_x] == 1 || grid[end_y][start_x] == 1 || grid[end_y][end_x] == 1) {
+
+    } else {
+        var line = layr.pathItems.add();
+        line.stroked = true;
+        line.filled = false;
+        line.strokeWidth = .25;
+
+        grid[start_y][start_x] = 1;
+        grid[end_y][start_x] = 1;
+        grid[end_y][end_x] = 1;
+
+        // line.setEntirePath( Array( Array(start_x*gridWH, start_y*gridWH), Array(start_x*gridWH, end_y*gridWH), Array(end_x*gridWH, end_y*gridWH) ) );
+        line.setEntirePath( Array( Array(start_x, start_y), Array(start_x, end_y), Array(end_x, end_y) ) );
+    }
+
+    app.redraw();
+}
+// alert (grid);
+
+function signed(a1, a2) {
+    if (a2 - a1 < 0) {
+        return -1;
+    } else {
+        return 1;
+    }
+}
+
+var layr2 = doc.layers.add();
+
+for (j=0; j<40; j++) {
+for (i=0; i<layr.pathItems.length; i++) {
+    var tY = layr.pathItems[i].pathPoints[0].anchor;
+    var tMid = layr.pathItems[i].pathPoints[1].anchor;
+    var tX = layr.pathItems[i].pathPoints[2].anchor;
+    // var yMove = Math.sign(tMid[1] - tY[1]);
+    // var xMove = Math.sign(tMid[0] - tX[0]);
+    var yMove = signed(tMid[1] , tY[1]);
+    var xMove = signed(tMid[0] , tX[0]);
+    tY[1] += yMove;
+    tX[0] += xMove;
+
+    if (grid[tY[1]][tY[0]] == 1 ) {
+        tY[1] -= yMove;
+        // var circle = layr2.pathItems.ellipse(tY[1], tY[0], .5, .5, true, false);
+        // circle.stroked = false;
+        // circle.filled = true;
+        // circle.fillColor = colCyan;
+        // alert (tY[1]+" y:x "+tY[0]);
+        // layr.pathItems[i].strokeColor = colPurple;
+    } else {
+        grid[tY[1]][tY[0]] = 1;
+    }
+
+    if (grid[tX[1]][tX[0]] == 1 ) {
+        tX[0] -= xMove;
+        // var circle = layr2.pathItems.ellipse(tX[1], tX[0], .25, .25, true, false);
+        // circle.stroked = false;
+        // circle.filled = true;
+        // circle.fillColor = colMagenta;
+        // alert (tY[1]+" y:x "+tY[0]);
+        // layr.pathItems[i].strokeColor = colGreen;
+    } else {
+        grid[tX[1]][tX[0]] = 1;
+    }
+
+    layr.pathItems[i].pathPoints[0].anchor = tY;
+    layr.pathItems[i].pathPoints[2].anchor = tX;
+    redraw();
+}
+}
+alert ("yay");
+// alert (grid);
